b497da5c105ec80d31146f583c4d92d083c85701
Jeff Q <jqiu1998@yahoo.com>
2014-07-19 21:55:36 -0400

    Stack Level Too Deep

--- pb2.rb
+++ pb2.rb
@@ -1,9 +1,17 @@
  #!/usr/bin/env rub
- 
-a=1
-b=1
-fib=0
-while fib<=4000000
-  b=a+b
-  a=a+b
-
+ a=0
+ def fib(n)
+   z=fib(n-1)+fib(n-2)
+   return z
+ end
+ n=2
+ answer=0
+ a=fib(n)
+ while a<= 4000000
+   if a%2==0
+     answer+=fib(n)
+    else 
+      n+=1
+    end
+ end
+ puts answer
