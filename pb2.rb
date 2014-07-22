8d2816b4bd0fce6ae920655eb355c683f6f0d621
Nick Marden <nick@marden.org>
2014-07-21 20:22:26 -0400

    Readability fixes

--- pb2.rb
+++ pb2.rb
@@ -2,18 +2,19 @@
 
 def fib(n)
   return n if n<=1
-  fib(n-1)+fib(n-2)
+  fib(n-1) + fib(n-2)
 end
-n=1
-answer=0
 
-while fib(n)< 4000000
-  a=fib(n)
-  if a%2==0
-    answer+= a
-    n+=1
+answer = 0
+n = 1
+while fib(n) < 4000000
+  a = fib(n)
+  if a%2 == 0
+    answer += a
+    n += 1
   else
-    n+=1
+    n += 1
   end
 end
+
 puts "The answer is #{answer}"
